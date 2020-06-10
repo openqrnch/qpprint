ee2a88581e6806f54bfb47f3eff0783365ef4953
Jan Danielsson <jan.danielsson@qrnch.com>
2020-06-10 16:20:26 +0200

    Add script for tagging releases.

---
+++ tag.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -e
+
+VERSION=`grep '^version' Cargo.toml | sed -E 's/^version[[:space:]]*=[[:space:]]*"([^"]+)"/\1/'`
+
+FSL_CHECKOUT=`fossil status | grep '^checkout:' | awk '{print $2}'`
+
+TAGNAME=qpprint-$VERSION
+
+fossil tag $TAGNAME $FSL_CHECKOUT
+fossil sync && fossil update
+
+git tag $TAGNAME
+git push origin $TAGNAME
+
+# vim: set ft=sh et sw=2 ts=2 sts=2 cinoptions=2 tw=79 :
